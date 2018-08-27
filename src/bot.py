a391d01cf99da04648966517fb8bef060cc2115e
unknown <cameronryan@eastlink.ca>
2018-08-27 18:09:18 -0300

    Remove unused Environ variables

--- src/bot.py
+++ src/bot.py
@@ -1,9 +1,15 @@
-import discord
+try:
+    import discord
+except:
+    from subprocess import check_output
+    check_output(['pip', 'install', '-U', 'https://github.com/Rapptz/discord.py/archive/rewrite.zip'])
+    import discord
+    
 from discord.ext import commands
 from os import environ
 
 AUTH_TOKEN = environ['DISCORD_TOKEN']
-REDIS_URL = environ['REDISTOGO_URL']
+#REDIS_URL = environ['REDISTOGO_URL']
 
 client = commands.Bot(command_prefix = '&')
 
