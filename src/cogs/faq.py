b86c21edbe3f69cad920e5fd6e337ce1b24728e5
Dylan <dylan.humber@gmail.com>
2018-09-21 18:04:25 -0300

    FAQ now matches wiki.

--- src/cogs/faq.py
+++ src/cogs/faq.py
@@ -7,62 +7,46 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
-whitelist = [
-    '106449616681062400', #Admin1
-    '107085990652280832', #Admin2
-    '209840589166870528' #me
-]
-
-class FaqModule:
-    def __init__(self, bot):
-        self.bot = bot
-        try:
-            self.questions = loads(self.bot.redis.get('questions'))
-            self.answers = loads(self.bot.redis.get('answers'))
-        except:
-            self.questions = list()
-            self.answers = list()
-
-    @commands.command(hidden=True)
-    async def faq_old(self, ctx): #Arbitrary number of arguments
-        '''Frequently assked questions.'''
-        wrapper = '```'
-        output = '{}Frequently asked questions:\n'.format(wrapper)
-
-        for index in range(0, len(self.questions)):
-            form_args = (index + 1, self.questions[index])
-            output += '{})\t{}\n'.format(*form_args)
-            
-        output += wrapper
-        await ctx.send(output)
-    
-    @commands.command(hidden=True)
-    async def faq_add(self, ctx, question):
-        if str(ctx.author.id) in whitelist:
-            self.questions.append(question)
-            self.bot.redis.set('questions', dumps(self.questions))
-            await ctx.send('Question added.')
-        else:
-            logging.info('{} requested an addition but is not whitelisted.'.format(ctx.author))
-
-    @commands.command()
-    async def faq(self, ctx):
-        FaqEmbed = Embed(
+FaqEmbed = Embed(
             title='Frequently Asked Questions',
+            url='http://evantyr.wiki/Guidelines',
             type='rich',
             author='Corbin',
             colour=Colour.from_rgb(23, 160, 101)
         )
-        for idx in range(0, len(self.questions)):
-            question = self.questions[idx]
-            
-            if idx < len(self.answers):
-                answer = self.answers[idx]
-            else:
-                answer = 'Null answer.'
 
-            FaqEmbed.add_field(name=question, value=answer, inline=False)
+faq = [
+    (
+        'How many characters am I allowed to have?',
+        'There is no limit to how many characters you can have. Just remember, the XP you earn is specific to your character!'
+    ),
+    (
+        'The character option I want was published in two books, which should I use?',
+        'The most recent publication of any given character option should be used.'
+    ),
+    (
+        'Can I trade items to other characters?',
+        'Yes. However, you can not trade items between two of your own characters. Any items traded, must end up in the possession of a different player.'
+    ),
+    (
+        "I'm playing a Cleric and/or some other religious character. What god(s) am I allowed to worship?",
+        "You can worship any god you like! We don't use any specific pantheon here. Just be sure to check Category:Deities first, and see if the god you wish to worship is listed there. If not, you can make a new page for it! Once a page for your god exists on the wiki, that god now exists within the lore of Evantyr!"
+    ),
+    (
+        "One of my characters has gone on a multi-session adventure. We haven't finished yet, but there is another adventure I'd like to play in. What do?",
+        "You can still play in other adventures while one of your characters is away on a multi-session adventure. However, your character can't be in two places at once. In order to play in the second adventure, you must play as a different character."
+    )
+]
 
+for pair in faq:
+    FaqEmbed.add_field(name=pair[0], value=pair[1], inline=False)
+
+class FaqModule:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def faq(self, ctx):
         await ctx.send(embed=FaqEmbed)
 
 
