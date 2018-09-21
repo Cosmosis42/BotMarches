c72c234505545f48f13fdd4c75e151dd04c6ca49
Dylan <dylan.humber@gmail.com>
2018-09-21 18:49:04 -0300

    Populate FAQ from wiki.

--- src/cogs/faq.py
+++ src/cogs/faq.py
@@ -4,6 +4,9 @@ from discord import Embed, Colour
 from discord.ext import commands
 from pickle import loads, dumps
 import logging
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
 
 logging.basicConfig(level=logging.INFO)
 
@@ -15,30 +18,27 @@ FaqEmbed = Embed(
             colour=Colour.from_rgb(23, 160, 101)
         )
 
-faq = [
-    (
-        'How many characters am I allowed to have?',
-        'There is no limit to how many characters you can have. Just remember, the XP you earn is specific to your character!'
-    ),
-    (
-        'The character option I want was published in two books, which should I use?',
-        'The most recent publication of any given character option should be used.'
-    ),
-    (
-        'Can I trade items to other characters?',
-        'Yes. However, you can not trade items between two of your own characters. Any items traded, must end up in the possession of a different player.'
-    ),
-    (
-        "I'm playing a Cleric and/or some other religious character. What god(s) am I allowed to worship?",
-        "You can worship any god you like! We don't use any specific pantheon here. Just be sure to check Category:Deities first, and see if the god you wish to worship is listed there. If not, you can make a new page for it! Once a page for your god exists on the wiki, that god now exists within the lore of Evantyr!"
-    ),
-    (
-        "One of my characters has gone on a multi-session adventure. We haven't finished yet, but there is another adventure I'd like to play in. What do?",
-        "You can still play in other adventures while one of your characters is away on a multi-session adventure. However, your character can't be in two places at once. In order to play in the second adventure, you must play as a different character."
-    )
-]
+pageUrl = 'http://evantyr.wiki/Guidelines'
+page = urlopen(pageUrl).read()
 
-for pair in faq:
+soup = BeautifulSoup(page, 'html.parser')
+
+unordered_lists = soup.findAll('ul')
+
+for ui in unordered_lists:
+    if "How many characters am I allowed to have?" in str(ui):
+        faq = ui.findAll('li')
+        break
+
+parsedFaq = list()
+for entry in faq:
+    question = str(entry.find('b'))
+    question = question.strip('</b>')
+    answer = str(entry.find('dd'))
+    answer = answer.strip('</d>')
+    parsedFaq.append((question, answer))
+
+for pair in parsedFaq:
     FaqEmbed.add_field(name=pair[0], value=pair[1], inline=False)
 
 class FaqModule:
