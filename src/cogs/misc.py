2c6c8a2ea5ba94f0cf4b7cdd2a9972b1e223b9bd
Dylan <dylan.humber@gmail.com>
2018-10-11 18:27:44 -0300

    gh command

--- src/cogs/misc.py
+++ src/cogs/misc.py
@@ -2,6 +2,8 @@
 
 from discord.ext import commands
 
+GUILDHALL = 483022288870965259
+
 class MiscModule:
     def __init__(self, bot):
         self.bot = bot
@@ -17,6 +19,12 @@ class MiscModule:
         """ECHO Echo echo
         echo..."""
         await ctx.send(message)
+
+    @commands.command(hidden=True)
+    async def gh(self, ctx, * ,message):
+        channel = self.bot.get_channel(GUILDHALL)
+        await channel.send(message)
+
 
 def setup(bot):
     bot.add_cog(MiscModule(bot))
