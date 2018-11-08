563645ceba7835b26c2ba054abaf7b4eb1b2b7df
Dylan <dylan.humber@gmail.com>
2018-11-07 22:01:50 -0400

    Dice rolling bug fix for multiple dice.

--- src/cogs/dice.py
+++ src/cogs/dice.py
@@ -17,7 +17,8 @@ class DiceModule:
         for a, b, c in matches: #a dice, b sides, c modifier
             c = c.replace(' ', '') #Remove white space incase someone has ' + 4' or such
             for dice in range(0, int(a)):
-                total += random.randint(1, int(b)) + int(c or 0)
+                total += random.randint(1, int(b))
+            total += int(c or 0)
 
         output = 'You rolled {}'.format(total)
         await ctx.send(output)                    
